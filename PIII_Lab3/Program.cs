acd0311b984361bd713f6c1426d8ebb01b6250dc
Bysiv01 <45607527+Bysiv01@users.noreply.github.com>
2020-10-24 14:20:43 +0200

    init

---
+++ PIII_Lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIII_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
